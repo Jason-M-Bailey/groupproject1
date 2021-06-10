9a979eb154b56a40c04967ef0aa907688bde49b8
mgpierrelouis <mgpierrelouis@gmail.com>
2021-06-10 15:45:59 -0400

    updated player search to feed in season averages for the 2021 nba season

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ var gifThree = document.getElementById("gif-3")
 var userSearch = document.getElementById("search-bar");
 var submitBtn = document.getElementById("submit-button");
 
+var playerName = document.getElementById("name");
+var points = document.getElementById("ppg");
+var rebounds = document.getElementById("rpg");
+var assists = document.getElementById("apg");
+var fieldGoal = document.getElementById("fg");
+var threes = document.getElementById("fg3p");
+var freeThrows = document.getElementById("ft");
+var gamesPlayed = document.getElementById("gp");
+var minutes = document.getElementById("mpg");
+
 let apiKey= "l638WPBsusN4Hth8cjVYu33qELrQ77E9" // api key created for nba project
 
 // https://api.giphy.com/v1/gifs/search?api_key=l638WPBsusN4Hth8cjVYu33qELrQ77E9&q=&limit=1&offset=0&rating=g&lang=en
@@ -14,7 +24,7 @@ let apiKey= "l638WPBsusN4Hth8cjVYu33qELrQ77E9" // api key created for nba projec
 // });
 
 function setup(){
-  let api = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=5&q=${userSearch.value}`;
+  let api = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=5&q=` + userSearch.value;
   fetch(api)
   .then(response => {
         return response.json();
@@ -32,10 +42,53 @@ function setup(){
       })
       .catch(err => console.log(err));
 }
+
+function playerSearch(){
+  var playerApi= 'https://www.balldontlie.io/api/v1/players?search='+ userSearch.value;
+  fetch(playerApi)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    console.log(json);
+    var playerID= json.data[0].id;
+    playerName.innerText= json.data[0].first_name + " " + json.data[0].last_name +" |" + json.data[0].team.abbreviation + "| 2020-2021 Stats";
+    var statsApi= 'https://www.balldontlie.io/api/v1/season_averages?api/v1/season_averages?season=2020&player_ids[]='+ playerID
+    fetch(statsApi)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json);
+      points.innerText = "PPG: " + json.data[0].pts;
+      rebounds.innerText= "RPG: " + json.data[0].reb;
+      assists.innerText= "APG: " + json.data[0].ast;
+      fieldGoal.innerText= "FG%: " + json.data[0].fg_pct;
+      threes.innerText= "3FG%: " + json.data[0].fg3_pct;
+      freeThrows.innerText="FT%: " + json.data[0].ft_pct;
+      gamesPlayed.innerText= "GP: " + json.data[0].games_played;
+      minutes.innerText= "MPG: " + json.data[0].min;
+
+
+
+      
+    }) 
+    
+  })
+
+  
+
+}
+
+
+
+
     
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault(); // prevent default behavior of submit button
   setup();
-  console.log(userSearch.value); 
+  playerSearch();
+  console.log(userSearch.value);
+   
 });
